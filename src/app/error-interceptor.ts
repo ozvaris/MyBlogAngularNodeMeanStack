a640801f7b360fa2c66b7599219bff44cf51ca22
ozvaris <ozgurozvaris@hotmail.com>
2019-07-21 14:40:02 +0300

    custom messages added

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -12,13 +12,17 @@ import { ErrorComponent } from "./error/error.component";
   @Injectable()
   export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(private dialog: MatDialog){}
+    constructor(private dialog: MatDialog) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
 
       return next.handle(req).pipe(
-          catchError((error : HttpErrorResponse) => {
-              this.dialog.open(ErrorComponent);
+          catchError((error: HttpErrorResponse) => {
+            let errorMessage = "An unknown Error Occured!";
+            if (error.error.message)  {
+              errorMessage = error.error.message;
+            }
+            this.dialog.open(ErrorComponent, {data:{message: errorMessage} });
               return throwError(error);
           })
       );
